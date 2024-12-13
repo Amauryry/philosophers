bd2ce1c0920971dc788a277f00f09d675f2c5624
Amaury Berion <aberion@made-f0Ar5s5.clusters.42paris.fr>
2024-12-13 13:45:27 +0100

    todo es bueno billynio

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: aberion <aberion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 07:54:47 by aberion           #+#    #+#             */
-/*   Updated: 2024/12/09 12:47:56 by aberion          ###   ########.fr       */
+/*   Updated: 2024/12/13 13:43:02 by aberion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,59 +15,60 @@
 
 # include <pthread.h>
 # include <stdio.h>
-# include <unistd.h>   // for usleep
-# include <stdlib.h>   // for malloc
-# include <sys/time.h> // for gettimeofday
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
 
 typedef struct s_data
 {
-    long statring_time;
-    int nb_p;
-    int die_time;     // Time until a philosopher dies if they don't eat
-    int eat_time;     // Time spent eating
-    int sleep_time;   // Time spent sleeping
-    int meal_nb;
-    pthread_mutex_t *forks;    // Array of forks (mutexes)
-    pthread_mutex_t *fork_status_mutex;
-    pthread_mutex_t *eaten_mutex;
-    pthread_mutex_t death_mutex;  // Mutex to protect the death flag
-    pthread_mutex_t print_mutex;  // Mutex to protect printf calls
-    pthread_mutex_t meal_mutex;
-    pthread_mutex_t meal_c_mutex;
-    int someone_died;  // Flag to indicate if a philosopher has died
-    int meal_checker;
-    int *fork_status;
-    int *eaten_enough;
-} t_data;
+	long			statring_time;
+	int				nb_p;
+	int				die_time;
+	int				eat_time;
+	int				sleep_time;
+	int				meal_nb;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*fork_status_mutex;
+	pthread_mutex_t	*eaten_mutex;
+	pthread_mutex_t	death_mutex;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	meal_mutex;
+	pthread_mutex_t	meal_c_mutex;
+	int				someone_died;
+	int				meal_checker;
+	int				*fork_status;
+	int				*eaten_enough;
+}					t_data;
 
 // Structure to hold the philosopher's ID and shared data
 typedef struct s_philosopher
 {
-    int id;
-    int meal_count;
-    t_data *data;
-    long last_meal_time;  // Timestamp of the last meal in milliseconds
-} t_philosopher;
+	int				id;
+	int				meal_count;
+	t_data			*data;
+	long			last_meal_time;
+}					t_philosopher;
 
-int	ft_atoi(const char *str);
-long get_current_time_in_ms();
-int skip_time(int time, t_data *data, t_philosopher *philo);
-void man_down(t_data *data, t_philosopher *philo);
-int     eat_routine(t_data *data, t_philosopher *philo);
-void lock_forks(t_data *data, int id);
-void unlock_forks(t_data *data, int id);
-int is_someone_dead(t_data *data);
-int sleep_routine(t_data *data, t_philosopher *philo);
-int launch_parsing(int argc, char **argv);
-int waiting_room(t_data *data, t_philosopher *philo);
-int check_meal(t_data *data);
-void set_meal(t_data *data, t_philosopher *philo);
-void	print_message(const char *s, t_data *data, t_philosopher *philo);
-int	handle_meal_check(t_data *data, t_philosopher *philo);
-int	handle_meal_state(t_data *data, t_philosopher *philo, int temp);
-int think_routine(t_data *data, t_philosopher *philo);
-void	setup_data(t_data *data, char **argv);
-void	cleanup_resources(t_data *data);
-
+int					ft_atoi(const char *str);
+long				get_current_time_in_ms(void);
+int					skip_time(int time, t_data *data, t_philosopher *philo);
+void				man_down(t_data *data, t_philosopher *philo);
+int					eat_routine(t_data *data, t_philosopher *philo);
+void				lock_forks(t_data *data, int id);
+void				unlock_forks(t_data *data, int id);
+int					is_someone_dead(t_data *data);
+int					sleep_routine(t_data *data, t_philosopher *philo);
+int					launch_parsing(int argc, char **argv);
+int					waiting_room(t_data *data, t_philosopher *philo);
+int					check_meal(t_data *data);
+void				set_meal(t_data *data, t_philosopher *philo);
+void				print_message(const char *s, t_data *data,
+						t_philosopher *philo);
+int					handle_meal_check(t_data *data, t_philosopher *philo);
+int					handle_meal_state(t_data *data, t_philosopher *philo,
+						int temp);
+int					think_routine(t_data *data, t_philosopher *philo);
+void				setup_data(t_data *data, char **argv);
+void				cleanup_resources(t_data *data);
 
 #endif
